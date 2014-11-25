0d0eba239f7a5ec0ace510fd7c5c66fac23cd3c6
Bryce <br.kalow@gmail.com>
2014-11-24 22:56:44 -0600

    Added index view, removed turbolinks, add username field, added login page

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   end
   resources :users
 
-  root to: 'users#index'
+  root to: 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121022248) do
+ActiveRecord::Schema.define(version: 20141124045903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20141121022248) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
