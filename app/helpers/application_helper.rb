6c06abc4d448865fe2c3690a54e44aac64d6633c
Bryce Kalow <br.kalow@gmail.com>
2014-12-01 23:11:39 -0600

    Updated nav link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
       link_to link_text, link_path
     end
   end
+
+  def nav_link_top(link_text, link_path, controller_name)
+    class_name = controller.controller_name == controller_name ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
+  def avatar_url(user, size)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -4,4 +4,10 @@ module SnippetsHelper
       return nav_link "My Snippets", snippets_mine_path 
     end
   end
+
+  def user_favorites_link
+    if user_signed_in?
+      return nav_link "My Favorites", snippets_favorites_path
+    end
+  end
 end
