6fbe7a6d44a5c38afca07681bdbd81b242ac60f4
Bryce <br.kalow@gmail.com>
2014-12-14 14:24:23 -0600

    User profile changed to tab view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_permalink(params[:id])
+
+    @snippets = @user.snippets.paginate(page: params[:s_page], per_page: 10)
+    @collections = @user.collections.paginate(page: params[:c_page], per_page: 10)
   end
 end
